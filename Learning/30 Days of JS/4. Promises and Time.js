b4267ab6e35cedb0d46ba4e6b0594a5ceb63fb33
mtnleo <mtnleonardi@gmail.com>
2024-06-11 15:22:25 -0300

    Started 4. Promises and Time in 30 days of js

---
+++ Learning/30 Days of JS/4. Promises and Time.js
@@ -0,0 +1,17 @@
+// 2723. Add two promises
+var addTwoPromises = async function(promise1, promise2) {
+    let x = await promise1;
+    let y = await promise2;
+    return new Promise((resolve, reject) => {
+        resolve(x + y);
+    })
+};
+
+// 2621. Sleep
+async function sleep(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout( () => {
+            resolve(true);
+        }, millis)
+    })
+}
